2bbb8658ccd8a6d75c6df2e455cc90a6ec19739d
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2024-02-06 09:36:31 -0300

    viniciusfcf

---
+++ participantes/viniciusfcf/README.md
@@ -0,0 +1,17 @@
+# Rinha de Backend 2024-Q1
+
+### Vinicius Ferraz
+
+Github: [@viniciusfcf](https://github.com/viniciusfcf)
+Twitter: [@viniciusfcf](https://twitter.com/viniciusfcf)
+Repositório: [https://github.com/viniciusfcf/rinha-de-backend-2024-q1/](https://github.com/viniciusfcf/rinha-de-backend-2024-q1)
+
+
+### Stack: Quarkus com Postgres
+- Quarkus 🚀
+- Postgres
+- Nginx
+
+### O que esperar?
+ - Vai dar ruim, na minha máquina para rodar sem erro, precisaria de mais CPU, algo entre 1.8 e 2
+ - Com 1.5 ficou com uma taxa de erro entre 10% e 15%
---
+++ participantes/viniciusfcf/src/main/java/org/acme/ClientesResource.java
@@ -0,0 +1,82 @@
+package org.acme;
+
+import java.time.LocalDateTime;
+
+import io.quarkus.narayana.jta.QuarkusTransaction;
+import jakarta.persistence.LockModeType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/clientes")
+public class ClientesResource {
+
+    @POST
+    @Path("/{id}/transacoes")
+    @Produces(MediaType.APPLICATION_JSON)
+    public LimiteSaldo debitarCreditar(@PathParam("id") Integer id, TransacaoEntrada te) {
+
+        if (!existeCliente(id)) {
+            throw new WebApplicationException(404);
+        }
+        if (!te.ehValida()) {
+            throw new WebApplicationException(422);
+        }
+        te.cliente_id = id;
+
+        Transacao t = Transacao.of(te);
+        QuarkusTransaction.begin();
+        SaldoCliente saldoCliente = SaldoCliente.findById(id, LockModeType.PESSIMISTIC_WRITE);
+        // TODO testar depois um find com lock e increment direto no java...
+        int valor = Integer.parseInt(te.valor);
+        if (te.tipo.equals("c")) {
+            saldoCliente.saldo += valor;
+        } else {
+            if (saldoCliente.saldo - valor < -saldoCliente.limite) {
+                QuarkusTransaction.rollback();
+                throw new WebApplicationException(422);
+            }
+            saldoCliente.saldo -= valor;
+        }
+        t.limite = saldoCliente.limite;
+        t.saldo = saldoCliente.saldo;
+        
+        saldoCliente.persist();
+        t.persist();
+        QuarkusTransaction.commit();
+        LimiteSaldo limiteSaldo = new LimiteSaldo(saldoCliente.saldo, saldoCliente.limite);
+        return limiteSaldo;
+    }
+
+    @GET
+    @Path("/{id}/extrato")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Extrato extrato(@PathParam("id") Integer id) {
+        if (!existeCliente(id)) {
+            throw new WebApplicationException(404);
+        }
+        Extrato extrato = new Extrato();
+        extrato.saldo.data_extrato = LocalDateTime.now();
+        extrato.ultimas_transacoes = Transacao.find("cliente_id = ?1 order by id desc", id).page(0, 10).list();
+        if (extrato.ultimas_transacoes.size() > 0) {
+            Transacao ultimaTransacao = extrato.ultimas_transacoes.get(0);
+            extrato.saldo.total = ultimaTransacao.saldo;
+            extrato.saldo.limite = ultimaTransacao.limite;
+        } else {
+            SaldoCliente saldoCliente = SaldoCliente.findById(id);
+            extrato.saldo.total = saldoCliente.saldo;
+            extrato.saldo.limite = saldoCliente.limite;
+        }
+        return extrato;
+    }
+
+    private boolean existeCliente(int id) {
+        return id > 0 && id < 6;
+        // return Cliente.findById(id) != null;
+    }
+}
